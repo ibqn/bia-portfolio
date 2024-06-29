8030757f5dc97e2068ef01d09f20c3a572187925
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-06-29 17:23:51 +0200

    add configuration files

---
+++ src/components/vue/counter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const counter = ref(0)
+const increment = () => {
+  console.log("increment")
+  counter.value++
+}
+</script>
+
+<template>
+  <div>
+    <h1>Counter</h1>
+    <p>Count: {{ counter }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
